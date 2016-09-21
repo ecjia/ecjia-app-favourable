1158c256136f9a674af871d2ec061211a3a65c5b
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-21 11:55:03 +0800

    商品列表增加商家筛选及商家搜索

--- classes/hooks/admin_favourable.class.php
+++ classes/hooks/admin_favourable.class.php
@@ -4,13 +4,11 @@ defined('IN_ECJIA') or exit('No permission resources.');
 class favourable_admin_plugin {
 	
 	static public function favourable_admin_menu_api($menus) {
-	    $menu = ecjia_admin::make_admin_menu('08_favourable_list', __('优惠活动'), RC_Uri::url('favourable/admin/init'), 8)->add_purview('favourable_manage');
+	    $menu = ecjia_admin::make_admin_menu('08_favourable_list', RC_Lang::get('favourable::favourable.favourable'), RC_Uri::url('favourable/admin/init'), 8)->add_purview('favourable_manage');
 	    $menus->add_submenu($menu);
 	    return $menus;
 	}
-	
 }
-
 
 RC_Hook::add_filter( 'promotion_admin_menu_api', array('favourable_admin_plugin', 'favourable_admin_menu_api') );
 
