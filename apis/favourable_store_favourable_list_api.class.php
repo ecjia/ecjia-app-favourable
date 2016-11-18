0cafbafbd9e91b94c624d3e5ce0fd0c9005e4e59
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-18 13:11:42 +0800

    优惠活动缓存修改

--- apis/favourable_store_favourable_list_api.class.php
+++ apis/favourable_store_favourable_list_api.class.php
@@ -17,48 +17,9 @@ class favourable_store_favourable_list_api extends Component_Event_Api {
     	$cache_id = sprintf('%X', crc32($cache_favourable_key));
     	$favourable_activity_db = RC_Model::model('favourable/orm_favourable_activity_model');
     	$favourable_list = $favourable_activity_db->get_cache_item($cache_id);
-    	
     	if (empty($favourable_list)) {
-    			$favourable_result = $db_favourable->where(array('store_id' => $options['store_id'], 'start_time' => array('elt' => RC_Time::gmtime()), 'end_time' => array('egt' => RC_Time::gmtime()), 'act_type' => array('neq' => 0)))->select();
-    			if (!empty($favourable_result)) {
-    				foreach ($favourable_result as $val) {
-    					if ($val['act_range'] == '0') {
-    						$favourable_list[] = array(
-    								'name' => $val['act_name'],
-    								'type' => $val['act_type'] == '1' ? 'price_reduction' : 'price_discount',
-    								'type_label' => $val['act_type'] == '1' ? __('满减') : __('满折'),
-    						);
-    					} else {
-    						$act_range_ext = explode(',', $val['act_range_ext']);
-    						switch ($val['act_range']) {
-    							case 1 :
-    								$favourable_list[] = array(
-    								'name' => $val['act_name'],
-    								'type' => $val['act_type'] == '1' ? 'price_reduction' : 'price_discount',
-    								'type_label' => $val['act_type'] == '1' ? __('满减') : __('满折'),
-    								);
-    								break;
-    							case 2 :
-    								$favourable_list[] = array(
-    								'name' => $val['act_name'],
-    								'type' => $val['act_type'] == '1' ? 'price_reduction' : 'price_discount',
-    								'type_label' => $val['act_type'] == '1' ? __('满减') : __('满折'),
-    								);
-    								break;
-    							case 3 :
-    								$favourable_list[] = array(
-    								'name' => $val['act_name'],
-    								'type' => $val['act_type'] == '1' ? 'price_reduction' : 'price_discount',
-    								'type_label' => $val['act_type'] == '1' ? __('满减') : __('满折'),
-    								);
-    								break;
-    							default:
-    								break;
-    						}
-    					}
-    				}
-    				$favourable_activity_db->set_cache_item($cache_id, $favourable_list);
-    			}
+    		$favourable_list = $db_favourable->where(array('store_id' => $options['store_id'], 'start_time' => array('elt' => RC_Time::gmtime()), 'end_time' => array('egt' => RC_Time::gmtime()), 'act_type' => array('neq' => 0)))->select();
+    		 $favourable_activity_db->set_cache_item($cache_id, $favourable_list);
     	}
     	return $favourable_list;
     }
