31f5a1c086474f476507ca5a01c29f039fe6f2eb
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-30 23:36:01 +0800

    指定ServiceProvider的路径

--- classes/FavourableServiceProvider.php
+++ classes/FavourableServiceProvider.php
@@ -9,7 +9,7 @@ class FavourableServiceProvider extends  AppParentServiceProvider
     
     public function boot()
     {
-        $this->package('ecjia/app-favourable');
+        $this->package('ecjia/app-favourable', null, dirname(__DIR__));
     }
     
     public function register()
